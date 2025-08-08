cf0b5625a979f09216dd53970320343eec3fbaeb
Cfberrio <98917677+Cfberrio@users.noreply.github.com>
2025-08-08 18:55:07 -0500

    Update taller1.js

--- taller1.js
+++ taller1.js
@@ -1,1 +1,49 @@
+//PUNTO 1
+function Farenheit (C) {
+    return (C * 9/5) + 32;
+}
+//PUNTO 2
+function formulageneral(a, b, c, pos) {
+    if (pos == true) {
+        return (-b() + Math.sqrt(b() * b() - 4 * a() * c())) / (2 * a());
+    } else {
+        return (-b() - Math.sqrt(b() * b() - 4 * a() * c())) / (2 * a());
+    }
+}
+
+// PUNTO 3 
+function paridad(num) {
+    if (num % 2 == 0){
+        return "par";
+    }else{
+        return "impar";
+    }
+}
+
+//PUNTO 4
+function peorparidad(num){
+    if (num == 1){
+        return "impar";
+    }else if (num == 2){
+        return "par";
+    }else if (num == 3){
+        return "impar";
+    }else if (num == 4){
+        return "par";
+    }else if (num == 5){
+        return "impar";
+    }else if (num == 6){
+        return "par";
+    }else if (num == 7){
+        return "impar";
+    }else if (num == 8){
+        return "par";
+    }else if (num == 9){
+        return "impar";
+    }else if (num == 10){
+        return "par";
+    }
+}
+
+
 
